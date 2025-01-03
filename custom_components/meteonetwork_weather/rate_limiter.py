3bd3a4f8936c3b252bdb71382228f4b53a8d2919
Davide Cavestro <davide.cavestro@gmail.com>
2025-01-03 21:35:30 +0100

    docs: document rate limiter methods

--- custom_components/meteonetwork_weather/rate_limiter.py
+++ custom_components/meteonetwork_weather/rate_limiter.py
@@ -3,14 +3,16 @@ import asyncio
 from datetime import datetime, timedelta
 
 class RateLimiter:
+    """API requests limiter."""
+
     def __init__(self, rate_limit=1):
+        """Initialize the rate limiter."""
         self.rate_limit = rate_limit  # Requests per second
         self._last_request_time = datetime.min
         self._lock = asyncio.Lock()
 
     async def throttle(self, request_callable):
-        """
-        Throttles execution of the given request callable based on rate limit.
+        """Throttles execution of the given request callable based on rate limit.
 
         :param request_callable: A callable (e.g., lambda) that performs the request.
         :return: The result of the callable.
