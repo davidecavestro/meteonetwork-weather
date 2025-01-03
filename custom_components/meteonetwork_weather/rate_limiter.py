93771481235277f680d2a3fb3b8b0f6f58c63bf5
Davide Cavestro <davide.cavestro@gmail.com>
2025-01-03 21:22:39 +0100

    fix: add missing rate_limiter

---
+++ custom_components/meteonetwork_weather/rate_limiter.py
@@ -0,0 +1,25 @@
+"""Limit requests to REST api endpoints from MeteoNetwork Weather component."""
+import asyncio
+from datetime import datetime, timedelta
+
+class RateLimiter:
+    def __init__(self, rate_limit=1):
+        self.rate_limit = rate_limit  # Requests per second
+        self._last_request_time = datetime.min
+        self._lock = asyncio.Lock()
+
+    async def throttle(self, request_callable):
+        """
+        Throttles execution of the given request callable based on rate limit.
+
+        :param request_callable: A callable (e.g., lambda) that performs the request.
+        :return: The result of the callable.
+        """
+        async with self._lock:
+            now = datetime.now()
+            wait_time = (self._last_request_time + timedelta(seconds=1 / self.rate_limit)) - now
+            if wait_time.total_seconds() > 0:
+                await asyncio.sleep(wait_time.total_seconds())
+
+            self._last_request_time = datetime.now()
+            return await request_callable()
